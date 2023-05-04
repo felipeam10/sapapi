e69dfde39099bfe7a1e9c83f63781a87942df2ad
felipeam10 <felipeam10@hotmail.com>
2023-05-04 15:43:19 -0300

    first commit

---
+++ src/main/java/com/apisap/sapapi/SapapiApplication.java
@@ -0,0 +1,16 @@
+package com.apisap.sapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapapiApplication.class, args);
+
+
+		System.out.println("vai dar certo");
+	}
+
+}
---
+++ src/main/java/com/apisap/sapapi/entities/Department.java
@@ -0,0 +1,39 @@
+package com.apisap.sapapi.entities;
+
+public class Department {
+
+    private Long id;
+    private String name;
+    private Double price;
+
+    public Department(){}
+
+    public Department(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
